d3452618538225c7ddfda1221ba450755d2cdf3d
saksatti <saksatti@gmail.com>
2024-09-21 15:02:53 -0400

    merge

--- whisper-wave/src/app/page.tsx
+++ whisper-wave/src/app/page.tsx
@@ -14,11 +14,11 @@ export default function Home() {
       <h2 className="text-black text-4xl text-center">A Toolkit  for the Deaf.</h2>
       <h3 className="text-black mt-16 mb-4 text-3xl text-center">Take a look at our features:</h3>
       <div className="flex flex-col justify-center items-center bg-gray-200 rounded-lg p-6">
-        <Button className="flex justify-center items-center m-2 text-black text-3xl hover:underline font-bold" variant="primary" href="/mediaProcessing">
+        <Button className="flex justify-center items-center m-2 text-black text-3xl hover:underline font-bold" variant="primary" href="/media-processing">
           <h1 className="mr-2">Media Processing</h1>
           <FontAwesomeIcon icon={faUpRightFromSquare} />
         </Button>
-        <Button className="flex justify-center items-center m-2 text-black text-3xl hover:underline font-bold" variant="primary" href="/speechAnalyzer">
+        <Button className="flex justify-center items-center m-2 text-black text-3xl hover:underline font-bold" variant="primary" href="/speech-analyzer">
           <h1 className="mr-2">Speech Analyzer</h1>
           <FontAwesomeIcon icon={faUpRightFromSquare} />
         </Button>
